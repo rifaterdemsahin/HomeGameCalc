b3eadfb9f4f57f944f0187d6c134cb36d9e6d89b
Rifat Erdem Sahin <rifaterdemsahin@gmail.com>
2016-10-08 14:06:05 +0300

    kerem asuman

---
+++ Assets/ChangeName.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+public class ChangeName : MonoBehaviour
+{
+    public string oyuncuName;
+    //Kendi methodlar!!!
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse clicked:Oyuncu " + oyuncuName);
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Game Started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //Debug.Log("Show FPS:" + DateTime.Now);
+       // Debug.Log("Show FPS");
+    }
+}
